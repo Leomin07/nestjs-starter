efd1408b6e2e6a90b658d4a64ff31475f46407b6
MinhTd <tranminshvp@gmail.com>
2023-12-27 16:44:27 +0700

    add property dto

--- src/helpers/utils.ts
+++ src/helpers/utils.ts
@@ -1,3 +1,5 @@
+import { LiteralObject } from '@nestjs/cache-manager';
+
 export function substrContent(content: string, index: number) {
   return content.length > index ? content.substring(0, index) + '...' : content;
 }
@@ -22,4 +24,50 @@ export function randomOTP(length = 6): string {
     result += digits[index];
   }
   return result;
+}
+
+export function returnPaging(
+  data: LiteralObject[],
+  totalItems: number,
+  params: LiteralObject,
+  metadata = {},
+) {
+  const totalPages = Math.ceil(totalItems / params.pageSize);
+  return {
+    paging: true,
+    hasMore: params.pageIndex < totalPages,
+    pageIndex: params.pageIndex,
+    totalPages: Math.ceil(totalItems / params.pageSize),
+    totalItems,
+    data,
+    ...metadata,
+  };
+}
+
+export function returnLoadMore(
+  data: LiteralObject[],
+  params: LiteralObject,
+  metadata = {},
+) {
+  return {
+    paging: true,
+    hasMore: data.length === params.pageSize,
+    data,
+    pageSize: params.pageSize,
+    ...metadata,
+  };
+}
+
+export function assignLoadMore(params: LiteralObject) {
+  params.pageSize = Number(params.pageSize) || 10;
+
+  return params;
+}
+
+export function assignPaging(params: LiteralObject) {
+  params.pageIndex = Number(params.pageIndex) || 1;
+  params.pageSize = Number(params.pageSize) || 10;
+  params.skip = (params.pageIndex - 1) * params.pageSize;
+
+  return params;
 }
