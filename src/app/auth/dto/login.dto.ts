696a8f6bc6e9d27555fc9f98aa57acc4c52653df
MinhTd <tranminshvp@gmail.com>
2023-12-27 16:44:27 +0700

    add property dto

--- src/app/auth/dto/login.dto.ts
+++ src/app/auth/dto/login.dto.ts
@@ -1,11 +1,14 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString, Length } from 'class-validator';
 
 export class LoginDto {
+  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   @Length(0, 20)
   phone: string;
 
+  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   @Length(0, 6)
--- src/app/auth/dto/register.dto.ts
+++ src/app/auth/dto/register.dto.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger';
 import {
   IsEnum,
   IsNotEmpty,
@@ -8,30 +9,36 @@ import {
 import { Gender } from 'src/helpers/enum';
 
 export class RegisterDto {
+  @ApiProperty()
   @IsOptional()
   @IsString()
   @Length(0, 6)
   code?: string;
 
+  @ApiProperty()
   @IsNotEmpty()
   @IsString()
   @Length(0, 20)
   phone: string;
 
+  @ApiProperty()
   @IsNotEmpty()
   @IsString()
   @Length(0, 255)
   registerToken: string;
 
+  @ApiProperty()
   @IsNotEmpty()
   @IsString()
   @Length(0, 255)
   name: string;
 
+  @ApiProperty({ enum: Gender })
   @IsNotEmpty()
   @IsEnum(Gender)
   gender: Gender;
 
+  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   birthday: string;
